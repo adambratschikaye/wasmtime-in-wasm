b342a9eaf27cb621fc513bb5bb38e0e854a51d2a
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2023-11-29 15:01:03 +0100

    execute _start

--- src/compare_compilation.rs
+++ src/compare_compilation.rs
@@ -1,7 +1,7 @@
 #[cfg(not(target_arch = "wasm32"))]
 mod go {
     use std::time::Instant;
-    use wasmtime::{Engine, Linker, Module, Store};
+    use wasmtime::{Config, Engine, Linker, Module, Store};
     use wasmtime_wasi::{Dir, WasiCtxBuilder};
 
     pub(super) fn go() {
@@ -11,29 +11,21 @@ mod go {
         let mut linker = Linker::new(&engine);
         wasmtime_wasi::add_to_linker(&mut linker, |s| s).unwrap();
 
-        println!(
-            "in host current dir has: {:?}",
-            std::fs::read_dir(".").unwrap().collect::<Vec<_>>()
-        );
         let wasi = WasiCtxBuilder::new()
-            .inherit_stdio()
-            .inherit_stderr()
+            // .inherit_stdio()
+            // .inherit_stderr()
             .preopened_dir(Dir::from_std_file(std::fs::File::open(".").unwrap()), ".")
             .unwrap()
             .build();
-        for i in 0..10 {
-            println!("wasi table entry {i}: {}", wasi.table().contains_key(i))
-        }
         let mut store = Store::new(&engine, wasi);
         let module = Module::from_file(&engine, compilation_wasm_path).unwrap();
         let instance = linker.instantiate(&mut store, &module).unwrap();
         let main = instance
-            .get_typed_func::<(), i32>(&mut store, "__main_void")
+            .get_typed_func::<(), ()>(&mut store, "_start")
             .unwrap();
 
         let start = Instant::now();
-        let result = main.call(&mut store, ()).unwrap();
-        assert_eq!(result, 0);
+        main.call(&mut store, ()).unwrap();
         println!("Compilation within wasmtime took {:?}", start.elapsed());
 
         let start = Instant::now();
@@ -43,7 +35,8 @@ mod go {
 
     fn compile_native() {
         let wat = std::fs::read("input.wat").unwrap();
-        let engine = Engine::default();
+        // let engine = Engine::default();
+        let engine = Engine::new(Config::default().parallel_compilation(false)).unwrap();
         let result = engine.precompile_module(&wat).unwrap();
         std::fs::write("serialized_module_native.cwasm", &result).unwrap();
     }
